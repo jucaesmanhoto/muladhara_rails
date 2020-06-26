1376e3ab451492d41c98331e85231f29ca0851f0
rafael osawa <osawa.rafael@gmail.com>
2020-06-26 18:28:12 +0300

    Appointment model

--- app/models/interagent.rb
+++ app/models/interagent.rb
@@ -1,5 +1,7 @@
 class Interagent < ApplicationRecord
   belongs_to :user
+
+  has_many :appointments
   # has_many :interagent_review
 
   # validates :birth_date, presence: true
--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,5 @@ class Room < ApplicationRecord
 
   has_many :room_pictures
   has_many :pictures, through: :room_pictures
+  has_many :appointments
 end
