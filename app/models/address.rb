c3f670b7ace6a3a35bdaf2fbb2b3fc5363745ce2
Victor Capelli <victorbustocapelli@hotmail.com>
2020-06-26 14:33:50 -0300

    references at address table

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,6 @@
 class Address < ApplicationRecord
   validates :zip_code, :street, :complement, :neighborhood, :city, :state, :country, presence: true
   validates :number, numericality: { only_integer: true }, presence: true
+  has_many :place_addresses
+  has_many :professional_addresses
 end
