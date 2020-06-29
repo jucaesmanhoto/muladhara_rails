fd8debffd86de8a9d2bcac3fe8f63562368fc94b
Yan Santos <yan17souza@hotmail.com>
2020-06-29 11:33:19 -0300

    initial home components

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @professionals = Professional.all
+    @places = Place.all
   end
 end
