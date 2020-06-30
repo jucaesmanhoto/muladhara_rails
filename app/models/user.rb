5bb95e8aeb74c453b24b30915d9330d66ff465a3
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-06-30 10:45:06 -0300

    putting some validations and associations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+  validates :name, :document, presence: true
+
+  has_many :appointments,through: :interagent, dependent: :destroy
+  has_many :places
+  has_many :interagents
+
+  has_one_attached :photo # Cloudinary
+
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
